49180b5e37e959c43e89db22f62dd686008db32f
brocode <kelimbth@gmail.com>
2023-06-20 10:43:25 +0300

    created user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from PIL import Image
 
-# Create your models here.
+class User(AbstractUser):
+    id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
+    email = models.EmailField(unique=True)
+    gender = models.CharField(max_length=7, blank=False)
+    phone_no = models.CharField(max_length=14, blank=False)
+    profile_pic = models.ImageField(upload_to='User-dps/', default='default.jpg')
+    updated = models.DateTimeField(auto_now=True)
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return f'{self.username}'
+    
+    def save(self, *args, **kwargs):
+        super(self, User).save(*args, **kwargs)
+
+        img = Image.open(self.profile_pic.path)
+
+        if img.height > 400 and img.width > 400:
+            output_size = (400, 400)
+            img.thumbnail(output_size)
+            img.save(self.profile_pic.path)
+    
+    class Meta:
+        ordering = ['username']
