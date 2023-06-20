24dfd59c4d4d67799a41601f5c9d0a01c858f15f
brocode <kelimbth@gmail.com>
2023-06-20 15:45:25 +0300

    added URL paths

---
+++ polls/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('new-poll/', views.CreatePollsView.as_view(), name='create-polls'),
+    path('choices/<str:pk>/create-choices/', views.CreateNewChoicesView.as_view(), name='create-choices'),
+    path('voting/<str:pk>/polls/', views.VotingView.as_view(), name='vote'),
+    path('<str:pk/edit/', views.EditPollsView.as_view(), name='edit-polls'),
+    path('choices/<str:pk>/edit/', views.EditChoicesView.as_view(), name='edit-choices'),
+
+]
