53f47ff2b0532ecc6cda0b05eadafd84281858af
brocode <kelimbth@gmail.com>
2023-06-20 12:47:08 +0300

    created profile and auth views

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from .forms import SignupForm, EditProfileForm
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+
+class SignupView(View):
+    form_class = SignupForm
+    template_name = 'accounts/signup.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+
+        context = {'SignupForm': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return redirect('profile')
+        
+        context = {'SignupForm': form}
+        return render(request, self.template_name, context)
+
+class UserProfileView(View):
+    form_class = EditProfileForm
+    template_name = 'accounts/profile.html'
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class(initial=self.initial)
+
+        context = {'EditProfileForm': form}
+        return render(request, self.template_name, context)
+    
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            return redirect('profile')
+        
+        context = {'EditProfileForm': form}
+        return render(request, self.template_name, context)
+
+
+class LogoutUser(LogoutView):
+    template_name = 'accounts/logout.html'
