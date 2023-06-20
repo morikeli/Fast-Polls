e5d17171020a8b99f9c34446743909ec07e11ff6
brocode <kelimbth@gmail.com>
2023-06-20 15:48:07 +0300

    updated INSTALLED_APPS

--- src/settings.py
+++ src/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
