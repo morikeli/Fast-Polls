59ecfec0c448e485e13999595a34c468fdb67f29
brocode <kelimbth@gmail.com>
2023-06-21 09:34:10 +0300

    deleted self.fields variable

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,7 +13,6 @@ class LoginForm(AuthenticationForm):
     def __init__(self, request=None, *args, **kwargs):
         super().__init__(request, *args, **kwargs)
 
-        self.fields['username'].label = 'Name'
         self.error_messages['invalid_login'] = 'INVALID CREDENTIALS!!! \
         Enter correct name (first name and last name -> separate first name and last name with a space!) and password. \
         Name and password maybe case-senstitive.'
