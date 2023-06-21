7e86e835619fd212371dcc8d8ddc20cfd2aea591
brocode <kelimbth@gmail.com>
2023-06-21 16:36:42 +0300

    updated EditPollsView URL path

--- polls/urls.py
+++ polls/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('new-poll/', views.CreatePollsView.as_view(), name='create-polls'),
     path('choices/<str:pk>/create-choices/', views.CreateNewChoicesView.as_view(), name='create-choices'),
     path('voting/<str:pk>/polls/', views.VotingView.as_view(), name='vote'),
-    path('<str:pk/edit/', views.EditPollsView.as_view(), name='edit-polls'),
+    path('<str:pk>/edit/', views.EditPollsView.as_view(), name='edit-polls'),
     path('choices/<str:pk>/edit/', views.EditChoicesView.as_view(), name='edit-choices'),
 
 ]
