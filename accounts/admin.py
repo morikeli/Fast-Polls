ab798a81ce9e45a1aba74d545d2f51c21ad7822b
brocode <kelimbth@gmail.com>
2023-06-21 13:27:05 +0300

    registered models in Admin Panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
+from django.db import models
+from .forms import SignupForm
+from .models import User
 
-# Register your models here.
+
+class UsersFormLayout(UserAdmin):
+    model = User
+    add_form = SignupForm
+    list_display = ['username', 'gender']
+    readonly_fields = ['gender', 'phone_no', 'profile_pic']
+    
+
+admin.site.register(User, UsersFormLayout)
--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,17 @@
+from .models import Questions, Choices, VotersDetails
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(Questions)
+class PollsTable(admin.ModelAdmin):
+    list_display = ['author', 'category', 'close_voting']
+    readonly_fields = ['author', 'category', 'close_voting']
+
+@admin.register(Choices)
+class ChoicesTable(admin.ModelAdmin):
+    list_display = ['choice', 'total_votes', 'edited']
+    readonly_fields = ['choice', 'total_votes']
+
+@admin.register(VotersDetails)
+class VotersTable(admin.ModelAdmin):
+    list_display = ['polls', 'voted']
+    
