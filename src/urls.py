f31601f3d9ada21944fcc5b3e85a879b310d0875
brocode <kelimbth@gmail.com>
2023-06-21 12:56:35 +0300

    appended URL paths

--- src/urls.py
+++ src/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
+    path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
