c3b3b3d09718d2f0f7f37f7dd64e60bbfc90ec50
brocode <kelimbth@gmail.com>
2023-10-16 16:42:34 +0300

    updated CreatePollsForms & EditPollsForm

--- polls/forms.py
+++ polls/forms.py
@@ -3,48 +3,96 @@ from django import forms
 
 class CreatePollsForm(forms.ModelForm):
     SELECT_CATEGORY = (
-        ('Public', 'Public'),
-        ('Private', 'Private'),
+        (None, '-- Select category --'),
+        ('Art', 'Art'),
+        ('Automotive', 'Automotive'),
+        ('Business', 'Business'),
+        ('Computing', 'Computing'),
+        ('Design', 'Design'),
+        ('Fashion', 'Fashion'),
+        ('Mathematics', 'Mathematics'),
+        ('Office', 'Office'),
+        ('Politics', 'Politics'),
+        ('Science', 'Science'),
+        ('Technology', 'Technology'),
+        ('Tips', 'Tips'),
+        ('Travel', 'Travel'),
     )
 
-    quiz = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': 'Type your question/poll here ...', 'class': 'mb-2'}),
+    quiz = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Type your question/poll here ...', 'class': 'mb-2',
+        }),
         label='Poll/Question',
     )
-    category = forms.ChoiceField(
-        widget=forms.Select(attrs={'type': 'select', 'class': 'mb-2'}),
+    category = forms.ChoiceField(widget=forms.Select(attrs={
+        'type': 'select', 'class': 'mb-2',
+        }),
         choices=SELECT_CATEGORY,
-        help_text='Indicate whether this is a public or private poll.'
+        help_text='Select one option to that is related to the subject of your poll.',
+    )
+    closing_dt = forms.DateField(widget=forms.DateInput(attrs={
+            'type': 'date', 'class': 'mb-0',
+        }),
+        label='Scheduled closing date',
+        help_text='Schedule a closing date for this poll. Once closed, voters will not vote.',
+    )
+    is_private = forms.BooleanField(widget=forms.CheckboxInput(attrs={
+        'type': 'checkbox', 'class': 'my-2',
+        }),
+        required=False,
+        help_text='Is this poll private? Leave blank if public.'
     )
 
     class Meta:
         model = Questions
-        fields = ['quiz', 'category']
+        fields = ['quiz', 'category', 'closing_dt', 'is_private']
 
 class EditPollsForm(forms.ModelForm):
     SELECT_CATEGORY = (
-        ('Public', 'Public'),
-        ('Private', 'Private'),
+        (None, '-- Select category --'),
+        ('Art', 'Art'),
+        ('Automotive', 'Automotive'),
+        ('Business', 'Business'),
+        ('Computing', 'Computing'),
+        ('Design', 'Design'),
+        ('Fashion', 'Fashion'),
+        ('Mathematics', 'Mathematics'),
+        ('Office', 'Office'),
+        ('Politics', 'Politics'),
+        ('Science', 'Science'),
+        ('Technology', 'Technology'),
+        ('Tips', 'Tips'),
+        ('Travel', 'Travel'),
     )
 
-    quiz = forms.CharField(
-        widget=forms.Textarea(attrs={'placeholder': 'Type your question/poll here ...', 'class': 'mb-2'}),
+    quiz = forms.CharField(widget=forms.Textarea(attrs={
+        'placeholder': 'Type your question/poll here ...', 'class': 'mb-2',
+        }),
         label='Poll/Question',
     )
-    category = forms.ChoiceField(
-        widget=forms.Select(attrs={'type': 'select'}),
+    category = forms.ChoiceField(widget=forms.Select(attrs={
+        'type': 'select', 'class': 'mb-2',
+        }),
         choices=SELECT_CATEGORY,
-        help_text='Indicate whether this is a public or private poll.'
+        help_text='Select one option to that is related to the subject of your poll.',
     )
-    close_voting = forms.BooleanField(
-        widget=forms.CheckboxInput(attrs={'type': 'checkbox', 'class': 'form-check-input'}),
+    closing_dt = forms.DateField(widget=forms.DateInput(attrs={
+            'type': 'date', 'class': 'mb-0',
+        }),
+        label='Scheduled closing date',
+        help_text='Schedule a closing date for this poll. Once closed, voters will not vote.',
+    )
+    is_private = forms.BooleanField(widget=forms.CheckboxInput(attrs={
+        'type': 'checkbox', 'class': 'mt-1',
+        }),
         required=False,
-        help_text='Select this if you wish to close this poll.'
+        help_text='Is this poll private? Leave blank if public.'
     )
 
     class Meta:
         model = Questions
-        fields = '__all__'
+        fields = ['quiz', 'category', 'closing_dt', 'is_private']
+
 
 class CreateMultipleChoicesForm(forms.ModelForm):
     choice = forms.CharField(
