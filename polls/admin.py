56af332c48194441bbd24f0452eff597cef6893b
brocode <kelimbth@gmail.com>
2023-10-16 16:41:56 +0300

    updated tables admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 
 @admin.register(Questions)
 class PollsTable(admin.ModelAdmin):
-    list_display = ['author', 'category', 'close_voting']
-    readonly_fields = ['quiz', 'category', 'close_voting']
+    list_display = ['author', 'category', 'closing_dt', 'is_private', 'is_closed']
+    readonly_fields = ['quiz', 'category', 'closing_dt', 'is_private', 'is_closed']
 
 @admin.register(Choices)
 class ChoicesTable(admin.ModelAdmin):
@@ -13,5 +13,5 @@ class ChoicesTable(admin.ModelAdmin):
 
 @admin.register(VotersDetails)
 class VotersTable(admin.ModelAdmin):
-    list_display = ['polls', 'voted']
-    
+    list_display = ['polls', 'voting_date']
+    readonly_fields = ['voter', 'polls', 'selected_choice']
