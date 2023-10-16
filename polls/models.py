5e41196af3b4f17a6ecbd2a28b0a0cae8f8bd60d
brocode <kelimbth@gmail.com>
2023-10-16 16:41:23 +0300

    updated models

--- polls/models.py
+++ polls/models.py
@@ -6,13 +6,15 @@ class Questions(models.Model):
     id = models.CharField(max_length=20, primary_key=True, unique=True, editable=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
     quiz = models.TextField(blank=False)
-    category = models.CharField(max_length=7, blank=False, default='Public')
-    close_voting = models.BooleanField(default=False)
+    category = models.CharField(max_length=15, blank=False)
+    is_private = models.BooleanField(default=False)
+    is_closed = models.BooleanField(default=False, editable=False)
+    closing_dt = models.DateField(null=False, blank=False, db_column='Scheduled closing date-time')
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'{self.author}'
+    def __str__(self) -> str:
+        return self.author
     
     class Meta:
         ordering = ['-created']
@@ -26,8 +28,8 @@ class Choices(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return f'{self.choice}'
+    def __str__(self) -> str:
+        return self.choice
     
     class Meta:
         ordering = ['total_votes']
@@ -36,14 +38,14 @@ class Choices(models.Model):
 class VotersDetails(models.Model):
     id = models.CharField(max_length=25, primary_key=True, unique=True, editable=False)
     polls = models.ForeignKey(Choices, on_delete=models.CASCADE, editable=False)
-    voter = models.ForeignKey(User, on_delete=models.CASCADE, editable=False)
+    voter = models.ForeignKey(User, on_delete=models.CASCADE, null=True, editable=False)
     selected_choice = models.CharField(max_length=100, editable=False)
-    voted = models.DateTimeField(auto_now_add=True)
+    voting_date = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return f'{self.polls}'
+    def __str__(self) -> str or None:
+        return self.voter
     
     class Meta:
-        ordering = ['polls']
+        ordering = ['polls', '-voting_date']
         verbose_name_plural = 'Voters Logs'
     
