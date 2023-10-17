28c149f9ccca5d7d2f43976f7247b5c7872011d8
brocode <kelimbth@gmail.com>
2023-10-17 11:23:58 +0300

    updated signal

--- polls/signals.py
+++ polls/signals.py
@@ -1,12 +1,17 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
 from .models import Questions, Choices, VotersDetails
+from datetime import datetime
 import uuid
 
 @receiver(pre_save, sender=Questions)
 def generate_questionID(sender, instance, **kwargs):
     if instance.id == '':
         instance.id = str(uuid.uuid4()).replace('-', '')[:20]
+    
+    # check if the closing datetime is due and mark the polls as closed - i.e no voting on the poll.
+    if instance.closing_dt >= datetime.now().strftime('%Y-%m-%d'):
+        instance.is_closed = True
 
 @receiver(pre_save, sender=Choices)
 def generate_choicesID(sender, instance, **kwargs):
