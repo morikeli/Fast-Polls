3ced1465cb5e86035fe2f6c5226bdebbab499e93
brocode <kelimbth@gmail.com>
2023-10-17 11:51:50 +0300

    added new url path

--- src/urls.py
+++ src/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
+from django.contrib import admin
 
 urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
+    re_path(r'^files/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
